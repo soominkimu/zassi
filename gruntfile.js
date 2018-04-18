588dca5c0d7186bf37c5780b39e37944967ac927
Soomin Kimu <soominkimu@gmail.com>
2018-04-18 02:27:56 -0700

    NFE 1st edition completed

---
+++ gruntfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+module.exports = function (grunt) {
+	grunt.initConfig({
+		// Watch task config
+		watch: {
+			sass: {
+				files: "sass/**",
+				tasks: ['sass']
+			}
+		},
+		// SASS task config
+		sass: {
+			options: {
+				sourceMap: false
+			},
+			dev: {
+				files: {
+					// destination : source file
+					"css/styles.css" : "sass/styles.scss"
+				}
+			}
+		},
+		// Post CSS
+		postcss: {
+			options: {
+				map: true,
+				processors: [
+					require('autoprefixer')({browsers: ['last 1 version']})
+				]
+			},
+			dist: {
+				src: 'css/*.css'
+			}
+		},
+		// Browser
+		browserSync: {
+			default_options: {
+				bsFiles: {
+					src: [
+						"*.html",
+						"css/*.css",
+					]
+				},
+				options: {
+					watchTask: true,
+					server: {
+						baseDir: "./"
+					}
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-browser-sync');
+
+	// Launch BrowserSync + watch task
+	grunt.registerTask('default', ['browserSync', 'watch']);
+};
